02926af8744f3475c9566851d51a153fd17b8c6d
Dolma <dolma1918@gmail.com>
2024-01-02 21:22:39 +0545

    feat: Removing the item from list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -23,8 +23,37 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added yet'),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: _groceryItems.length,
+          itemBuilder: (context, index) => Dismissible(
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                key: ValueKey(_groceryItems[index].id),
+                child: ListTile(
+                    leading: Container(
+                      width: 23,
+                      height: 23,
+                      color: _groceryItems[index].category.color,
+                    ),
+                    trailing: Text(_groceryItems[index].quantity.toString()),
+                    title: Text(
+                      groceryItems[index].name,
+                    )),
+              ));
+    }
     return Scaffold(
         appBar: AppBar(
           title: const Text("Your Groceries"),
@@ -32,17 +61,6 @@ class _GroceryListState extends State<GroceryList> {
             IconButton(onPressed: _addItem, icon: const Icon(Icons.add))
           ],
         ),
-        body: ListView.builder(
-            itemCount: _groceryItems.length,
-            itemBuilder: (context, index) => ListTile(
-                leading: Container(
-                  width: 23,
-                  height: 23,
-                  color: _groceryItems[index].category.color,
-                ),
-                trailing: Text(_groceryItems[index].quantity.toString()),
-                title: Text(
-                  groceryItems[index].name,
-                ))));
+        body: content);
   }
 }
