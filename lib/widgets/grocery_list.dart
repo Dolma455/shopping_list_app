58c9c575b531b5e5877566f4a41f9110654b752a
Dolma <dolma1918@gmail.com>
2024-01-12 21:08:08 +0545

    chore: Handling no data case

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -33,6 +33,12 @@ class _GroceryListState extends State<GroceryList> {
     if (response.statusCode >= 400) {
       _error = 'Failed to fetch data please try again';
     }
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -64,20 +70,18 @@ class _GroceryListState extends State<GroceryList> {
   }
 
   void _removeItem(GroceryItem item) async {
-    final index=_groceryItems.indexOf(item);
-     setState(() {
+    final index = _groceryItems.indexOf(item);
+    setState(() {
       _groceryItems.remove(item);
     });
     final url = Uri.https('backendhttp-cb680-default-rtdb.firebaseio.com',
         'shopping-list/${item.id}.json');
-    final response=await http.delete(url);
-    if(response.statusCode>=400) {
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
       setState(() {
         _groceryItems.insert(index, item);
       });
     }
-
-   
   }
 
   @override
